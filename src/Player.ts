19f4f62daaf8d39e45ce6645413e5930beafaa1f
Paul Barrass <paul.barrass@gmail.com>
2018-11-02 00:05:46 +0000

    Only align with slope on first contact

--- src/Player.ts
+++ src/Player.ts
@@ -10,6 +10,7 @@ import {
    Quaternion,
    TransformNode,
    Axis,
+   FollowCamera,
 } from 'babylonjs'
 
 const down = Vector3.Down()
@@ -31,6 +32,8 @@ export default class Player extends GameObject {
          this.scene
       )
       this.mesh.setPositionWithLocalVector(new Vector3(0, 5, 2))
+
+      this.game.camera.lockedTarget = this.mesh!
       //this.mesh.position.y = 5
       //this.mesh.position.z = 2
    }
@@ -42,21 +45,23 @@ export default class Player extends GameObject {
       this.ray.direction = down
       const result = this.ray.intersectsMesh(this.game.track.mesh!, false)
       if (result.hit) {
+         const distanceToFloor = result.distance
          const normal = result.getNormal(true, true)!
-         this.mesh!.lookAt(position.add(normal))
          const across = Vector3.Cross(normal, Vector3.Down())
          const downhill = Vector3.Cross(across, normal)
-         const distanceToFloor = result.distance
          if (distanceToFloor <= touchingDistance) {
             if (!this.wasTouchingGroundLastFrame) {
-               this.wasTouchingGroundLastFrame = true
-               // just touching for first time, convert
                //   console.log('touched ground!')
+               this.wasTouchingGroundLastFrame = true
+               this.mesh!.lookAt(position.add(downhill))
+               position.y += distanceToFloor - touchingDistance
+               this.velocity = downhill.scale(delta / 12)
+            } else {
+               // continuing to touch ground
             }
-            this.velocity = downhill.scale(1)
-            // touching ground
          } else {
             this.wasTouchingGroundLastFrame = false
+            this.mesh!.lookAt(position.add(downhill))
             //  console.log('mid air!', this.velocity)
             // mid air, just apply gravity
             this.velocity.addInPlace(
@@ -65,7 +70,12 @@ export default class Player extends GameObject {
          }
 
          //
-         this.mesh!.position.addInPlace(this.velocity)
+      } else {
+         // no ground beneath us, round fail for now
+         console.log('game over')
       }
+
+      // apply the velocity
+      this.mesh!.position.addInPlace(this.velocity)
    }
 }
