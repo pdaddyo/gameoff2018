c22d9068abfb573c986cfd4f0d6834a0cf8c7561
Paul Barrass <paul.barrass@gmail.com>
2018-11-03 19:21:02 +0000

    Shorten posts and remember cornerStartY

--- src/InteractablePost.ts
+++ src/InteractablePost.ts
@@ -6,17 +6,20 @@ export default class InteractablePost extends Interactable {
    height = 10
    directionMultiplier = 0
    wasRotatingLastFrame = false
+   cornerStartY = 0
    constructor(
       postIndex: number,
       position: Vector3,
       range: number,
+      cornerStartY: number,
       directionMultiplier: number
    ) {
       super(range)
       this.directionMultiplier = directionMultiplier
+      this.cornerStartY = cornerStartY
       this.mesh = MeshBuilder.CreateCylinder(
          `post${postIndex}`,
-         { height: 18, diameterTop: 0, diameterBottom: 4 },
+         { height: 8, diameterTop: 0, diameterBottom: 4 },
          this.scene
       )
       this.mesh.position = position.add(new Vector3(0, this.height / 2, 0))
