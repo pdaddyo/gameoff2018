a3f67f988a41e72bd6b759ac3f124c22cd994d76
Paul Barrass <paul.barrass@gmail.com>
2018-11-05 21:45:47 +0000

    Use sync in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 s3cmd put -P index.html s3://gameoff.dreamm.co.uk/
-s3cmd put -P ./dist/compiled.js s3://gameoff.dreamm.co.uk/dist/compiled.js
-
+s3cmd -P sync ~/git/gameoff2018/dist/ s3://gameoff.dreamm.co.uk/dist/
